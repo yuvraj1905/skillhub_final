fb069cd8185cfb3ab185cc33ff094d94a1d47cd5
yuvraj1905 <94983216+yuvraj1905@users.noreply.github.com>
2023-05-12 11:35:35 +0530

    Revert "fo"
    This reverts commit 2765249d59cdc1b5c477b64123b0846bed25c9da.

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
-import Razorpay from "razorpay";
-import nodeCorn from "node-cron";
-import { Stats } from "./models/StatsModel.js";
+import RazorPay from "razorpay";
+import nodeCron from "node-cron";
+import { Stats } from "./models/Stats.js";
 connectDB();
 
-const PORT = process.env.PORT;
-
-// Cloudinary Config
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-// RazorPay Instance
-export const instance = new Razorpay({
+export const instance = new RazorPay({
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
-nodeCorn.schedule("0 0 0 1 * *", async () => {
+nodeCron.schedule("0 0 0 5 * *", async () => {
   try {
     await Stats.create({});
   } catch (error) {
