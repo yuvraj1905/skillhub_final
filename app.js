38a492a05ca9e54f5056fa0be2507efc448487d5
yuvraj1905 <94983216+yuvraj1905@users.noreply.github.com>
2023-05-12 10:52:52 +0530

    fo

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 import express from "express";
-import { config } from "dotenv";
-import ErrorMiddleware from "./middlewares/Error.js";
+import dotenv from "dotenv";
+import colors from "colors";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import ErrorMiddleware from "./middleware/ErrorMiddleware.js";
+dotenv.config();
 
-config({
-  path: "./config/config.env",
-});
 const app = express();
 
-// Using Middlewares
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -25,12 +19,13 @@ app.use(
   })
 );
 
-// Importing & Using Routes
+// All Routes Import
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
 import payment from "./routes/paymentRoutes.js";
 import other from "./routes/otherRoutes.js";
 
+// All Routes prefix
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
@@ -40,7 +35,7 @@ export default app;
 
 app.get("/", (req, res) =>
   res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    `<h1>Site is Working. Click <a href=${process.env.FRONTEND_URL} > here </a> to visit frontend </h1>`
   )
 );
 
